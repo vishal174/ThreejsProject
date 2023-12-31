c1f6a0ef1abf46e0f9aa125dbfcf84b41f8073cd
vishal174 <sailaxmivishal456@gmail.com>
2023-12-31 14:29:36 +0530

    first commit

---
+++ react/src/App.jsx
@@ -0,0 +1,15 @@
+import Home from './pages/Home'
+import Canvas from './canvas'
+import Customizer from './pages/Customizer'
+function App() {
+
+  return (
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
+  )
+}
+
+export default App
